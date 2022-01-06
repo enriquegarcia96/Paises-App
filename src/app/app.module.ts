1081cb6f0e11a8bb5a4c63cfb629b9fdda36f1b0
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-06 11:53:30 -0600

    refactor: 💡 exportando los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
@@ -9,7 +9,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, PaisModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    PaisModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
