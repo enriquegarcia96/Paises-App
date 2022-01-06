f1717170409d0d5461dad44c6444136e96934c82
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-06 11:57:15 -0600

    refactor: 💡 mostrar sugerencias al escribir

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,16 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
-  ]
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  buscar(termino: string) {
+    this.mostrarSugerencias = false;
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(this.termino).subscribe({
+      next: (paises) => (console.log(paises), (this.paises = paises)),
+      error: (error) => (
+        (this.hayError = true), (this.paises = []), console.log(error)
+      ),
+    });
   }
 
+  /**
+   * para el input
+   */
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais(termino).subscribe({
+      next: (paises) => (this.paisesSugeridos = paises.splice(0, 5)),
+      error: (error) => (this.paisesSugeridos = []),
+    });
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+  }
 }
