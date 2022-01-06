b0029f639f9a5c9252d4224acaefbd1316ea70b4
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-06 12:07:46 -0600

    feat: 🎸 propiedad de entrada para mostrar los paises

---
+++ src/app/pais/components/pais-tabla/pais-tabla.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+
+@Component({
+  selector: 'app-pais-tabla',
+  templateUrl: './pais-tabla.component.html',
+  styleUrls: [],
+})
+export class PaisTablaComponent {
+  @Input() paises: Country[] = [];
+}
