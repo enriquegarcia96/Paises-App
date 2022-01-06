967ced07b5da2c7bf7b2a57dbbcfefac391ef728
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-01-06 12:00:07 -0600

    refactor: 💡 funciones para obtener los paises por regiones

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 
+  activarRegion(region: string) {
+    if (region === this.regionActiva) {
+      return;
+    }
+
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisService
+      .buscarRegion(this.regionActiva)
+      .subscribe((regiones) => (this.paises = regiones));
+  }
 }
